1f7f115d5a744484bd895c6805e38201c96feea0
vargaae <vargaae@hotmail.com>
2023-06-30 00:49:09 +0200

    clean up and init Card

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div>
+        <img alt='React' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' height='200' />
+        <div>
+        <h2>React</h2>
+        <p>v18</p></div>
+        </div>
+    );
+}
+
+export default Card
